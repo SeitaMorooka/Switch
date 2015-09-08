702eda441f1153a74e06008280543e45aaae4cc5
師岡誠太 <3624@ezweb.ne.jp>
2015-09-08 12:57:06 +0900

    新規登録

---
+++ switch.playground/Contents.swift
@@ -0,0 +1,31 @@
+var singou = "red"
+
+switch singou {
+    case "red":
+    println("stop")
+    
+    case "yellow":
+    println("carefully")
+    
+    case "blue":
+    println("go")
+default:
+    println("malfunction")
+}
+
+var FavoriteSite = "Twitter"
+
+switch FavoriteSite {
+    case "Facebook"
+    println("Like!")
+    
+    case "Twitter":
+    println("Tweet")
+    
+default:
+    println("Why?")
+    
+}
+
+
+
